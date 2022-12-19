c96d061c2480f8d576c75d9d2ce3101c8323153b
Carrington <Manyuchi>
2022-12-19 15:16:44 +0200

    Initial Commit

---
+++ CarringtonCard/CarringtonCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarringtonCardApp.swift
+//  CarringtonCard
+//
+//  Created by Carrington Tafadzwa Manyuchi on 2022/12/19.
+//
+
+import SwiftUI
+
+@main
+struct CarringtonCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarringtonCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarringtonCard
+//
+//  Created by Carrington Tafadzwa Manyuchi on 2022/12/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
